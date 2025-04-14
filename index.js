45cd5e8f61f3749617e895e2b8f3c54be07dfde4
JustinParviz <jarshravan@gmail.com>
2025-04-14 14:36:17 -0700

    built out the nextSlide and showSlide functions

--- index.js
+++ index.js
@@ -16,6 +16,17 @@ function initializeSlider() {
 
 function showSlide(index) {
 
+    if (index >= slides.length) {
+        slideIndex = 0;
+    }
+    else if (index < 0) {
+        slideIndex = slides.length - 1;
+    }
+
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide");
+    });
+    slides[slideIndex].classList.add("displaySlide");
 }
 
 function prevSlide() {
@@ -23,7 +34,8 @@ function prevSlide() {
 }
 
 function nextSlide() {
-
+    slideIndex++;
+    showSlide(slideIndex);
 }
 
 
